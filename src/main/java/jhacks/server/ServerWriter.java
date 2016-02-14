b6a22c9fd440e8fc71d1a1d76f9cf3c73b666461
Jordan Brown <jmbrown@andrew.cmu.edu>
2016-02-14 02:52:02 -0500

    server writer

---
+++ src/main/java/jhacks/server/ServerWriter.java
@@ -0,0 +1,13 @@
+package jhacks.server;
+
+import java.net.Socket;
+
+public class ServerWriter implements Runnable {
+  private Socket client1;
+  private Socket client2;
+  
+  @Override
+  public void run() {
+  }
+
+}
