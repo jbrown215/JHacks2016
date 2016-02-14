06a892e61deeb826b26f1b1b6524e8686bfce117
Jacob Best <kevingarnett95@Jacob-Bests-MacBook-Pro.local>
2016-02-13 22:44:47 -0500

    Adding market.java

---
+++ src/main/java/jhacks/server/Market.java
@@ -0,0 +1,12 @@
+package jhacks.server;
+
+public class Market {
+	
+	public void buy();
+	public void sell();
+	public void connect();
+	public void cancel();
+	public void unpack();
+	public void pack();
+
+}
