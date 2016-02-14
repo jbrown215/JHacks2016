24e7fe19697878f97b5dc1834700eacb9974b3fa
rosenad <rosena2795@gmail.com>
2016-02-14 01:45:56 -0500

    adding gui

--- src/main/java/jhacks/GUI/Main.java
+++ src/main/java/jhacks/GUI/Main.java
@@ -1,65 +1,92 @@
 package jhacks.GUI;
+
 import javax.swing.SwingUtilities;
+
+import java.awt.Color;
+import java.util.Map;
+import java.util.TreeMap;
+
 import javax.swing.JFrame;
 import org.jfree.chart.*;
+import org.jfree.chart.plot.Plot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYDataItem;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 
 public class Main {
-	static String stock = null;		
+	static String stock = null;	
+	static JFreeChart chart;
 	static XYDataset data;
-	static int count = 0;
-//	static XYSeries currentData;
-//	static XYSeries newData;
+	static int key = 0;
+	static int Yvalue = 0;
 	static XYSeries series;
-	String name;
+	static String name;
+	static Map<String,Integer[]> chartInfo;
+	static Integer[] keyAndYvalue;
+	static XYSeriesCollection seriesCollection;
+	static XYSeriesCollection thisSeries;
 
 
 
-	public static void main(){
+
+	public static void main(String args[]) {
+		
+	}
+	public static void main(String name, Double price){
 		JFrame frame = new JFrame("Chart");
+//		this.name = name;
+		chartInfo = new TreeMap<String, Integer[]>();
+		keyAndYvalue = new Integer[2];
+
 		
 		frame.setSize(500,500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		
-		String name = null;
-		double price = 0;
-		setChartName(name);
-		setStock(name, price);
-		data = createDataset();
-		JFreeChart chart = createChart(data);
+		if(!chartInfo.containsKey(name)){
+			keyAndYvalue[0] = key;
+			keyAndYvalue[1] = 0;
+			chartInfo.put(name, keyAndYvalue);
+			key++;
+		}
+		
+		
+		chart = upDateChart(price);
+		
 		ChartPanel chartPanel = new ChartPanel(chart);
+//		setContentPane(chartPanel);
+		
+		
+	}
 
-		
-		
-	}
-	static void setChartName(String name){
-	}
 	
-	static XYDataset createDataset(){
-		series = new XYSeries("price");
-//		setStock();
+	static JFreeChart upDateChart(double price){
+		int i = chartInfo.get(name)[0]; //key
+		if(seriesCollection.getSeries(i) == null){
+			series = new XYSeries(name);
+			series.add(1,price);
+
+			chartInfo.put(name, keyAndYvalue);
+		}else{
+		 series = seriesCollection.getSeries(i);
+		 keyAndYvalue[0] = chartInfo.get(name)[0];
+		 keyAndYvalue[1] = chartInfo.get(name)[1]++;
+		 chartInfo.put(name, keyAndYvalue);
+		 series.add((double)chartInfo.get(name)[1], price);
+		}
 		
-		
-		return null;
-		
-	}
-	
-	
-	static void setStock(String name, double price){
-//		for (int i = 0; i < count; i++){
-//			XYSeries series = new XYSeries("price");
-//			series.add(i, currentData.getY(i));
-//		}
-		series.add(count, price);
-		count++;
-	}
-	
-	static JFreeChart createChart(XYDataset dataSet){
+		seriesCollection.addSeries(series);
+		thisSeries = new XYSeriesCollection();
+		thisSeries.addSeries(series);
 		JFreeChart chart = ChartFactory.createXYLineChart(stock,"Time", "Price", 
-				data, PlotOrientation.VERTICAL, true, true, false);
+				(XYDataset)thisSeries, PlotOrientation.VERTICAL, true, true, false);
+		chart.setBackgroundPaint(Color.WHITE);
+//		XYPlot plot = chart.getXYPlot();
+		
 		return chart;
 	}
 }
