9f5c454bd3ff30b7a4b87c6e0a7c807c54bf5efe
Jordan Brown <jmbrown@andrew.cmu.edu>
2016-02-14 11:49:37 -0500

    modified market to attempt to make trades (poorly)

--- src/main/java/jhacks/server/Market.java
+++ src/main/java/jhacks/server/Market.java
@@ -9,18 +9,21 @@ import java.util.Set;
 
 import jhacks.utils.Pair;
 
-
 public class Market {
-  // Map from stock name to a pair, consisting of the list of BUY and SELL orders
+  // Map from stock name to a pair, consisting of the list of BUY and SELL
+  // orders
   // LEFT is buy, RIGHT is sell
   private Map<String, Pair<List<Order>, List<Order>>> marketInfo = new HashMap<>();
   // Name of all the stocks in this game instance
   private Set<String> stockNames = new HashSet<String>();
-  
+
+  private User client1;
+  private User client2;
+
   public Market(Set<String> stockNames) {
     this.stockNames = stockNames;
   }
-  
+
   public void addBuyOrder(String name, double price, int quantity) {
     System.out.println("adding buy order: " + name);
     if (marketInfo.get(name) == null) {
@@ -28,19 +31,84 @@ public class Market {
       ArrayList<Order> asks = new ArrayList<Order>();
       marketInfo.put(name, new Pair<List<Order>, List<Order>>(bids, asks));
     }
-    
+
     Pair<List<Order>, List<Order>> securities = marketInfo.get(name);
-    securities.getLeft().add(new Order(name, price, quantity));
+    Order order = new Order(name, price, quantity);
+    securities.getLeft().add(order);
+    attemptToMakeTrade(order, true);
   }
-  
+
   public void addSellOrder(String name, double price, int quantity) {
     if (marketInfo.get(name) == null) {
       ArrayList<Order> bids = new ArrayList<Order>();
       ArrayList<Order> asks = new ArrayList<Order>();
       marketInfo.put(name, new Pair<List<Order>, List<Order>>(bids, asks));
     }
-    
+
     Pair<List<Order>, List<Order>> securities = marketInfo.get(name);
-    securities.getRight().add(new Order(name, price, quantity));
+    Order order = new Order(name, price, quantity);
+    securities.getRight().add(order);
+    attemptToMakeTrade(order, false);
+  }
+
+  /**
+   * This function is not correct yet. It will only fill orders with larger
+   * quantities than the bid instead of filling it incrementally.
+   * 
+   * @param order
+   * @param isBuy
+   */
+  public void attemptToMakeTrade(Order order, boolean isBuy) {
+    if (isBuy) {
+      for (Order other : marketInfo.get(order.getName()).getRight()) {
+        if (other.getQuantity() >= order.getQuantity() && other.getPrice() <= order.getPrice()) {
+          // update user holdings
+          if (client1.getOrders().contains(order)) {
+            client1.getOrders().remove(order);
+            client2.getOrders().remove(other);
+          } else {
+            client2.getOrders().remove(order);
+            client1.getOrders().remove(other);
+          }
+
+          if (client2.getOrders().contains(order)) {
+            client2.getOrders().remove(order);
+            client1.getOrders().remove(other);
+
+          } else {
+            client1.getOrders().remove(order);
+            client2.getOrders().remove(other);
+          }
+          // update orders
+          System.out.println("TODO update order");
+        }
+        break;
+      }
+    } else {
+      for (Order other : marketInfo.get(order.getName()).getLeft()) {
+        if (other.getQuantity() >= order.getQuantity() && other.getPrice() >= order.getPrice()) {
+          // update user holdings
+          // update orders
+          if (client1.getOrders().contains(order)) {
+            client1.getOrders().remove(order);
+            client2.getOrders().remove(other);
+          } else {
+            client2.getOrders().remove(order);
+            client1.getOrders().remove(other);
+          }
+
+          if (client2.getOrders().contains(order)) {
+            client2.getOrders().remove(order);
+            client1.getOrders().remove(other);
+
+          } else {
+            client1.getOrders().remove(order);
+            client2.getOrders().remove(other);
+          }
+        }
+        System.out.println("TODO update order");
+      }
+
+    }
   }
 }
